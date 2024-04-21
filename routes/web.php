de861fd38b04878601a5378559971c8439ddcf43
romhappy <romainbesin21@gmail.com>
2024-04-21 21:29:28 +0200

    fix name routes

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home.home');
-});
+})->name('app_home');
 
 Route::get('/about', function () {
     return view('home.about');
-});
+})->name('app_about');
