325dc212fe902eef90ad644006587c5ee3dd2437
romhappy <romainbesin21@gmail.com>
2024-04-27 15:35:34 +0200

    style hero

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -1,3 +1,12 @@
 <x-layout>
-    <h1>Home page</h1>
+    <div class="hero_background">
+    <div class="hero_container">
+        <div>
+            <p>Kijami: éclairer l'inclusion par des évaluations précises.</p>
+            <p class="hero_subtitle">un outil open-source à destination des professionnels et des familles pour
+                évaluer la situation et les besoins de personnes en situation de handicap. </p>
+            </div>
+            <img src="{{ URL::to('/') }}/img/hero_img.svg" class="hero_img" alt="logo du site">
+        </div>
+    </div>
 </x-layout>
