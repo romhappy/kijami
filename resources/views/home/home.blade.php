a8c498f6a5acb92dca36ad92c4ec07e82a16d718
romhappy <romainbesin21@gmail.com>
2024-04-28 21:49:00 +0200

    add things front

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -3,14 +3,15 @@
         <div class="hero_container">
             <div>
                 <p>Présenter clairement et simplement une situation de handicap.</p>
-                <p class="hero_subtitle">Kijami est un outil open-source destiné à aider les personnes en situation de handicap,
+                <p class="hero_subtitle">Kijami est un outil open-source destiné à aider les personnes en situation de
+                    handicap,
                     les professionnels et les familles à communiquer sur les situations de handicap et les besoins qui
                     en résultent. </p>
             </div>
             <img src="{{ URL::to('/') }}/img/hero_img.svg" class="hero_img" alt="logo du site">
         </div>
     </div>
-    <div class="card_title_container">
+    <div class="homepage_subtitle">
         <h1>A qui s'adresse Kijami?</h1>
     </div>
     <div class="cards_container">
@@ -32,5 +33,26 @@
             <h2 class="text_card">Préciser une demande d'accès aux droits, présenter une situation dans le cadre d'une
                 demande d'admission, garantir un bon relai d'accompagnement, ...</h2>
         </div>
+    </div>
+    <div class="explain_container">
+        <div class="sub_explain_container">
+            <img src="{{ URL::to('/') }}/img/nav_logo.svg" class="nav_logo" alt="logo du site">
+            <p>Kijami a été élaboré par des professionnels en activité au sein d'organisations médico-sociales. En
+                combinant les
+                évaluations les plus couramment utilisées pour évaluer l'autonomie (telles que les GIR, GEVA, GEVASCO,
+                etc.)
+                avec une multitude d'autres outils de collecte de données (tels que les dossiers d'admission, les
+                rapports
+                d'opportunité, les questionnaires sur les vacances adaptées, etc.),Kijami offre un accompagnement pour
+                identifier et comprendre les besoins dans le contexte d'un handicap.
+        </div>
+        <ul>
+            <li>Repas</li>
+            <li>Sommeil</li>
+            <li>Hygiène</li>
+            <li>Habillage</li>
+            <li>Communication</li>
+            <li>Activités</li>
+        </ul>
     </div>
 </x-layout>
