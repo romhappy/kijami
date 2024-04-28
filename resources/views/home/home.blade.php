e14a24fa3964d8fd52cba6b57a581a0f1b5e1913
romhappy <romainbesin21@gmail.com>
2024-04-28 14:32:36 +0200

    add cards

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -1,12 +1,33 @@
 <x-layout>
     <div class="hero_background">
-    <div class="hero_container">
-        <div>
-            <p>Eclairer l'inclusion par des évaluations précises.</p>
-            <p class="hero_subtitle">un outil open-source à destination des professionnels et des familles pour
-                évaluer la situation et les besoins de personnes en situation de handicap. </p>
+        <div class="hero_container">
+            <div>
+                <p>Eclairer l'inclusion par des évaluations précises.</p>
+                <p class="hero_subtitle">Un outil open-source destiné à aider les personnes en situation de handicap,
+                    les professionnels et les familles à communiquer sur les situations de handicap et les besoins qui
+                    en résultent. </p>
             </div>
             <img src="{{ URL::to('/') }}/img/hero_img.svg" class="hero_img" alt="logo du site">
+        </div>
+    </div>
+    <div class="cards_container">
+        <div class="card">
+            <img src="{{ URL::to('/') }}/img/personn_card.svg" class="img_card"
+                alt="logo personne en situation de handicap">
+            <h1 class="card_title">Personnes en situation de handicap</h1>
+            <h2 class="text_card">Informer par soi-même, préciser une demande d'accès au droits, ...</h2>
+        </div>
+        <div class="card">
+            <img src="{{ URL::to('/') }}/img/pro_card.svg" class="img_card" alt="logo professionnel de santé">
+            <h1 class="card_title">Professionnels</h1>
+            <h2 class="text_card">Affiner son intervention, alimenter un projet personnalisé, transmettre de façon
+                précise, harmoniser les pratiques, ...</h2>
+        </div>
+        <div class="card">
+            <img src="{{ URL::to('/') }}/img/family_card.svg" class="img_card" alt="logo familles">
+            <h1 class="card_title">Familles</h1>
+            <h2 class="text_card">Préciser une demande d'accès aux droits, présenter une situation dans le cadre d'une
+                demande d'admission, garantir un bon relai d'accompagnement, ...</h2>
         </div>
     </div>
 </x-layout>
