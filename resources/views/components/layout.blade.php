3f31e2d6787cc5502248ea890a89eea16f3d3ba0
romhappy <romainbesin21@gmail.com>
2024-05-02 21:44:12 +0200

    add login page

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,14 +7,9 @@
 
     <title> @yield('title')</title>
 
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
     <!-- Styles -->
     <link href="{{ URL::asset('css/style.css') }}" rel="stylesheet">
-    <link href="{{ URL::asset('css/login.css') }}" rel="stylesheet">
-
 </head>
 
 <body>
